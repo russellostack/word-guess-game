315974bc206d390825c59cd6dac611f39e304d54
Russell Stack <russellostack@gmail.com>
2019-02-12 13:39:42 -0500

    html started, js file reformatted

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,15 @@
 var wordBank = ["hills","mountain","stone","rocks","badger", "range","ridge","boulder","pebbles","sand","earthquake","landslide"];
 var guesses = [];
-var word = words[Math.floor(Math.random() * words.length)];
-word.forEach(function(i){
-    guesses[i]="_";
-});
+
+var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+var wins = 0;
+var guessesLeft = word.length +6;
+
+for (var i=0; i<word.length;i++){
+    guesses[i] = "_";
+}
+
 var remainingLetters = word.length
 while (remainingLetters > 0){
     alert(guesses.join(" "));
@@ -15,14 +21,20 @@ while (remainingLetters > 0){
         alert ("Please enter only 1 letter.")
     } 
     else {
-        word.length.forEach(function(j){
-            if (word[j] === guess) {
+        for (var j=0; j<word.length;j++){
+            if (word[j]===guess){
                 guesses[j] = guess;
                 remainingLetters--;
+                
             }
-        });
+        }
+
     }
-    alert(guesses.join(" "));
-    alert("Good job the answer was " + word);
+    
 
 }
+alert(guesses.join(" "));
+alert("Good job the answer was " + word);
+console.log("var word " + word);
+console.log("var wordList " + wordBank);
+console.log("var remainingLetters " + remainingLetters);
