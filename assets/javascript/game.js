58209d69132c1f32b51e2bad2da073304c103eab
Russell Stack <russellostack@gmail.com>
2019-02-12 17:00:06 -0500

    barebones, mostly working, prompt wont start after page loads

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,48 @@
 var wordBank = ["hills","mountain","stone","rocks","badger", "range","ridge","boulder","pebbles","sand","earthquake","landslide"];
 var guesses = [];
-
-var word = wordBank[Math.floor(Math.random() * wordBank.length)];
-
+var word = "";
 var wins = 0;
-var guessesLeft = word.length +6;
+var guessesLeft = 1;
+var wrongCounter = [];
 
-for (var i=0; i<word.length;i++){
+function startGame(){
+    guesses = [];
+    word = wordBank[Math.floor(Math.random() * wordBank.length)];
+    guessesLeft = word.length +5;
+    for (var i=0; i<word.length;i++){
     guesses[i] = "_";
-}
+    }
+    var remainingLetters = word.length
+    document.querySelector("#guesses").innerHTML = guesses.join(" ");
+    document.querySelector("#guessCounter").innerHTML = ("Guesses Remaining: " + guessesLeft);
+    // document.querySelector("#winCounter").innerHTML = ("Games Won: " + wins);
+    document.querySelector("#wrongGuesses").innerHTML = ("Wrong Guesses: "+ wrongCounter);
 
-var remainingLetters = word.length
-while (remainingLetters > 0){
-    alert(guesses.join(" "));
-    var guess = prompt("Guess a letter!")
-    if (guess ===null){
-        break;
-    } 
-    else if (guess.length !== 1){
-        alert ("Please enter only 1 letter.")
-    } 
-    else {
-        for (var j=0; j<word.length;j++){
-            if (word[j]===guess){
-                guesses[j] = guess;
-                remainingLetters--;
-                
+    while (remainingLetters > 0 && guessesLeft > 0){
+        var guess = prompt("Guess a letter!")
+        if (guess ===null){
+            break;
+        } 
+        else if (guess.length !== 1){
+            alert ("Please enter only 1 letter.")
+        } 
+        else {
+            for (var j=0; j<word.length;j++){
+                if (word[j]===guess){
+                    guesses[j] = guess;
+                    remainingLetters--;
+                    
+                }
+                else {
+                    wrongCounter.push(word[j])
+                }
             }
+
         }
+        
 
     }
-    
-
 }
-alert(guesses.join(" "));
-alert("Good job the answer was " + word);
-console.log("var word " + word);
-console.log("var wordList " + wordBank);
-console.log("var remainingLetters " + remainingLetters);
+startGame()
+console.log(wrongCounter);
+console.log(word);
