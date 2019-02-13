2dcd0375bdc4e2e938a8aa1ecab35b8dcaf2b0ce
russellostack <32462495+russellostack@users.noreply.github.com>
2019-02-12 21:16:11 -0500

    last minute changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,60 @@
-$(document).ready(function(){
+
 var wordBank = ["hills","mountain","stone","rocks","badger", "range","ridge","boulder","pebbles","sand","earthquake","landslide"];
-var guesses = [];
 var word = "";
+var lettersOfWord = [];
+var blanks = 0;
+var blanksAndCorrect = [];
+var wrongBank = [];
 var winCounter = 0;
-var guessesLeft = 1;
-var wrongCounter = [];
-function check(){
-    if (guess ===null){
+var guessesLeft = 10;
+
+function game(){
+    word = wordBank[Math.floor(Math.random() * wordBank.length)];
+    lettersOfWord = word.split("");
+    blanks = lettersOfWord.length;
+
+    for (var i=0; i<word.length;i++){
+        blanksAndCorrect.push("_");
+    }
+    document.getElementById("#currentWord").innerHTML = " "+ blanksAndCorrect.join(" ");
+
+    while (guessesLeft > 0){
+        check(letterGuessed);
+    }
+}
+function check(letterGuessed){
+    if (letterGuessed ===null){
                 break;
     } 
-    else if (guess.length !== 1){
-        alert ("Please enter only 1 letter.")
+    else if (letterGuessed.length !== 1){
+        alert ("Please choose only 1 letter.")
     } 
     else {
-        for (var j=0; j<word.length;j++){
-            if (word[j]===guess){
-                guesses[j] = guess;
-                remainingLetters--;
-                
+        for (var j=0; j<blanks;j++){
+            if (word[j]===letterGuessed){
+                blanksAndCorret[j] = letterGuessed;                
             }
             else {
-                wrongCounter.push(guess);
+                wrongBank.push(letterGuessed);
+                guessesLeft--;
             }
         }
     }
+    document.querySelector("#guessesLeft").innerHTML = ("Guesses Remaining: " + guessesLeft);
+    document.querySelector("#winCounter").innerHTML = ("Games Won: " + winCounter);
+    document.querySelector("#wrongBank").innerHTML = ("Wrong Guesses: "+ wrongBank);
+    document.querySelector("currentWord").innerHTML = ("Current Word: "+ blanksAndCorrect);
 }
 function game(){
-    guesses = [];
-    word = wordBank[Math.floor(Math.random() * wordBank.length)];
-    guessesLeft = word.length +5;
-    for (var i=0; i<word.length;i++){
-    guesses[i] = "_";
-    }
-    var remainingLetters = word.length;
-    while (remainingLetters > 0 && guessesLeft > 0){
-        var guess = letterGuessed);
-        check(guess)
-        
-        document.querySelector("#guesses").innerHTML = guesses.join(" ");
-        document.querySelector("#guessCounter").innerHTML = ("Guesses Remaining: " + guessesLeft);
-        document.querySelector("#winCounter").innerHTML = ("Games Won: " + winCounter);
-        document.querySelector("#wrongGuesses").innerHTML = ("Wrong Guesses: "+ wrongCounter);
-
-    }
-}
-function roundComplete(){
 
 }
+game()
 document.onkeyup = function(event){
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-    game(letterGuessed);
-    roundComplete();
-startGame()
+    check(letterGuessed);
+    gameEnd();
+
+}
 console.log(wrongCounter);
 console.log(word);
-});
+
