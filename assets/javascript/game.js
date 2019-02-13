ff149afe685a8c746bb58b643664a2325d516169
russellostack <32462495+russellostack@users.noreply.github.com>
2019-02-12 22:15:41 -0500

    display problems solved, need to get game started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var lettersOfWord = [];
 var blanks = 0;
 var blanksAndCorrect = [];
 var wrongBank = [];
+
 var winCounter = 0;
 var guessesLeft = 10;
 
@@ -16,45 +17,32 @@ function game(){
     for (var i=0; i<word.length;i++){
         blanksAndCorrect.push("_");
     }
-    document.getElementById("#currentWord").innerHTML = " "+ blanksAndCorrect.join(" ");
-
-    while (guessesLeft > 0){
-        check(letterGuessed);
-    }
+    document.getElementById("currentWord").innerHTML = " "+ blanksAndCorrect.join(" ");
 }
 function check(letterGuessed){
-    if (letterGuessed ===null){
-                break;
-    } 
-    else if (letterGuessed.length !== 1){
-        alert ("Please choose only 1 letter.")
-    } 
-    else {
-        for (var j=0; j<blanks;j++){
-            if (word[j]===letterGuessed){
-                blanksAndCorret[j] = letterGuessed;                
-            }
-            else {
-                wrongBank.push(letterGuessed);
-                guessesLeft--;
-            }
+    for (var j=0; j<blanks;j++){
+        if (word[j]===letterGuessed){
+            blanksAndCorret[j] = letterGuessed;                
+        }
+        else {
+            wrongBank.push(letterGuessed);
+            guessesLeft--;
         }
     }
-    document.querySelector("#guessesLeft").innerHTML = ("Guesses Remaining: " + guessesLeft);
-    document.querySelector("#winCounter").innerHTML = ("Games Won: " + winCounter);
-    document.querySelector("#wrongBank").innerHTML = ("Wrong Guesses: "+ wrongBank);
-    document.querySelector("currentWord").innerHTML = ("Current Word: "+ blanksAndCorrect);
+    
+    document.getElementById("guessesLeft").innerHTML = ("Guesses Remaining: " + guessesLeft);
+    document.getElementById("wrongBank").innerHTML = ("Wrong Guesses: "+ wrongBank);
+    document.getElementById("currentWord").innerHTML = ("Current Word: "+ blanksAndCorrect);
 }
-function game(){
+function (){
 
 }
 game()
 document.onkeyup = function(event){
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
     check(letterGuessed);
-    gameEnd();
 
+    document.getElementById("winCounter").innerHTML = ("Games Won: " + winCounter);
 }
-console.log(wrongCounter);
 console.log(word);
 
